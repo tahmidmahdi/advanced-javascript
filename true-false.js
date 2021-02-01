5973a1df27fcd7bb170f7398d0d228902f59bd50
tahmidmahdi <tahmid5550@gmail.com>
2021-02-01 23:15:46 +0600

    truthy and falsy values are elaborately described

---
+++ true-false.js
@@ -0,0 +1,28 @@
+const age = 0;
+if(age){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+const name = '';
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is flase");
+}
+
+//Falsy
+//0
+//''
+//undefined
+//null
+//NaN
+//Truthy
+//'0'
+//[] empty string also true
+//{} empty obj is also true
+// false is false but
+//'false' is true because it is a string
